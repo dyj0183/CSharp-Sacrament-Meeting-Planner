b918acc9f0e6f3ff70276aaaf111edd7a9bbfacf
Yu-Chun Tung <dyj0183@byui.edu>
2021-07-06 11:00:55 -0600

    updated page model and page views with proper order and display names, added CSS for table and buttons

---
+++ SacramentMeetingPlanner/Migrations/20210706155000_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SacramentMeetingPlanner.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SacramentMeeting",
+                columns: table => new
+                {
+                    SacramentMeetingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfMeeting = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConductLeader = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OpenHymn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CloseHymn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SacramentHymn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OpenPrayer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClosePrayer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MusicalNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SacramentMeeting", x => x.SacramentMeetingId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SacramentMeeting");
+        }
+    }
+}
--- SacramentMeetingPlanner/Models/SacramentMeeting.cs
+++ SacramentMeetingPlanner/Models/SacramentMeeting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,38 @@ namespace SacramentMeetingPlanner.Models
     public class SacramentMeeting
     {
         // properties
+       
         public int SacramentMeetingId { get; set; } // primary key
+
+        [Display(Name = "Date")]
+        [Required]
         public DateTime DateOfMeeting { get; set; }
+
+        [Display(Name = "Conducting Leader")]
+        [Required]
         public string ConductLeader { get; set; }
+
+        [Display(Name = "Opening Hymn")]
+        [Required]
         public string OpenHymn { get; set; }
+
+        [Display(Name = "Closing Hymn")]
+        [Required]
         public string CloseHymn { get; set; }
+
+        [Display(Name = "Sacrament Hymn")]
+        [Required]
         public string SacramentHymn { get; set; }
+
+        [Display(Name = "Opening Prayer")]
+        [Required]
         public string OpenPrayer { get; set; }
+
+        [Display(Name = "Closing Prayer")]
+        [Required]
         public string ClosePrayer { get; set; }
+
+        [Display(Name = "Musical Number")]
         public string MusicalNumber { get; set; } // optional
     }
 }
