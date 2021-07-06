844492f2dc4663ac9ce7dc65a3fa2ffa68374e46
Yu-Chun Tung <dyj0183@byui.edu>
2021-07-06 12:51:56 -0600

    tried to add speaker into a specific sacrament, still having issues and bugs

--- SacramentMeetingPlanner/Controllers/SpeakersController.cs
+++ SacramentMeetingPlanner/Controllers/SpeakersController.cs
@@ -60,7 +60,7 @@ namespace SacramentMeetingPlanner.Controllers
             {
                 _context.Add(speaker);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("/SacramentMeetings/create");
             }
             return View(speaker);
         }
