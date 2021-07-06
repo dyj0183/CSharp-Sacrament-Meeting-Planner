d988e38ed976b1fe7aa0610c4177af0ea1462264
Yu-Chun Tung <dyj0183@byui.edu>
2021-07-06 09:35:34 -0600

    scaffold SacramentMeeting Pages

---
+++ SacramentMeetingPlanner/Models/SacramentMeeting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class SacramentMeeting
+    {
+        // properties
+        public int SacramentMeetingId { get; set; } // primary key
+        public DateTime DateOfMeeting { get; set; }
+        public string ConductLeader { get; set; }
+        public string OpenHymn { get; set; }
+        public string CloseHymn { get; set; }
+        public string SacramentHymn { get; set; }
+        public string OpenPrayer { get; set; }
+        public string ClosePrayer { get; set; }
+        public string MusicalNumber { get; set; } // optional
+    }
+}
